fc4a16cb0aed54d409514b2112ce3b60e9c0becb
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:51:09 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://gitlab.freedesktop.org/rncbc/qpwgraph -b v0.4.2
 rm -rfv ./qpwgraph/debian
@@ -12,8 +7,6 @@ cd ./qpwgraph
 LOGNAME=root dh_make --createorig -y -l -p qpwgraph_0.4.2
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
